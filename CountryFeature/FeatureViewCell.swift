b39ce164e8081b96077b59e3a52bd71c9a807b30
lakiando@outlook.com <lakiando@outlook.com>
2017-05-09 12:16:00 -0700

    new files for table view

---
+++ CountryFeature/FeatureViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureViewCell.swift
+//  CountryFeature
+//
+//  Created by Lazar Andnov on 5/9/17.
+//  Copyright © 2017 Lazar Andnov. All rights reserved.
+//
+
+import UIKit
+
+class FeatureViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
