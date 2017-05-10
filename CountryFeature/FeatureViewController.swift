ae3bd2d30f6a028f56c41ef4b5dfd33757a22c41
lakiando@outlook.com <lakiando@outlook.com>
2017-05-10 08:30:33 -0700

    created a LaunchScreen

--- CountryFeature/FeatureViewController.swift
+++ CountryFeature/FeatureViewController.swift
@@ -15,12 +15,6 @@ class FeatureViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.HTTP()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     func HTTP() {
@@ -43,9 +37,6 @@ class FeatureViewController: UITableViewController {
             
         task.resume()
     }
-
-
-
 
     // MARK: - Table view data source
 
